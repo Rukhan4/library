abe8915654ea74eb689937646b4cefc908052c38
Surur Khan <surur.rkhan@gmail.com>
2022-06-10 16:42:51 -0400

    Ability to add book but no response yet

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ closeForm.addEventListener('click', () => {
 
 const addBook = document.querySelector('#submit');
 addBook.addEventListener('click', addBookToLibrary);
+
+// Storing new books into an array
+
+let myLibrary = [];
+
+function Book() {
+    this.title = form.title.value;
+    this.author = form.author.value;
+    this.pages = form.pages.value + 'pg';
+    this.read = form.read.checked;
+}
+
+function addBookToLibrary(e) {
+    e.preventDefault();
+    popUpForm.style.display = none;
+
+    let currentBook = new Book();
+    myLibrary.push(currentBook);
+}
